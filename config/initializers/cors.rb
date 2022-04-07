ef9c6fb01d80c7531c9fac30ad1f67abdd1dc179
Gary Lin <garyqwertyuiop@gmail.com>
2022-04-07 14:47:22 +0800

    allow local host 4200 request

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.action_controller.forgery_protection_origin_check = false
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost"
+    origins "localhost:4200"
 
     resource "*",
       headers: :any,
